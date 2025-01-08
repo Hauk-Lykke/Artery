78a86403464cb630f87108acaa77d0cf1f98f770
Hauk-Morten <haly@cowi.com>
2025-01-08 11:34:25 +0100

    New abstract Path and Branch classes added with Branch2D inheriting

--- src/routing.py
+++ src/routing.py
@@ -1,28 +1,72 @@
+from core import Node
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import List, Tuple
 from queue import PriorityQueue
-from src.components import FloorPlan
-from src.pathfinding import Pathfinder
-from src.visualization import visualize_layout, visualize_routing
+from structural import FloorPlan
+from pathfinding import Pathfinder
+from visualization import PathfindingVisualizer, visualize_layout
 
-def route_ducts(floor_plan: FloorPlan, test_name: str = None):
-	if floor_plan.ahu is None:
-		raise ValueError("AHU must be set in floor plan before routing ducts")
+
+class Path:
+	'''Class for storing Mechanical, Electrical and Plumbing routes. Each bend/fitting is a node.'''
+	def __init__(self, startNode: Node, goal: Node=None):
+		self.startNode = startNode
+		self.endNode = None
+		self.nodes = [startNode]
+		self.length = 0
+		self.cost = self.nodes[-1].g
+		self.goal = goal
 	
-	pathfinder = Pathfinder(floor_plan)
-	furthest_room = pathfinder.find_furthest_room(floor_plan.ahu)
+	def maxCost(self) -> float:
+		# Create a copy to avoid modifying original list
+		nodesSortedByCost = self.nodes.copy()
+		# Fix lambda syntax and sort in descending order
+		nodesSortedByCost.sort(key=lambda x: x.g_cost, reverse=True)
+		# Return the highest cost (first element)
+		return nodesSortedByCost[0].g_cost if nodesSortedByCost else 0
+
+	def append(self, node: Node):
+		self.nodes.append(node)
+		self.startNode = node
+
+	def __getitem__(self,index: int) -> Node:
+		return self.nodes[index]
+
+class Branch(Path): # Mechanical, Electrical, Plumbing branch
+	def __init__(self, startNode: Node, isIndexRoute: bool = False):
+		super().__init__(startNode)
+		self.sub_branches = []
+		self.isIndexRoute = isIndexRoute
+
+class Branch2D(Path):
+	def __init__(self, floorPlan: FloorPlan, startNode: Node, isIndexRoute: bool = False):
+		super().__init__(startNode, isIndexRoute)
+		self.floorPlan = floorPlan
+		self.figure = None # Figure handle
+		self.axes = None # Figure axes
+
+	def generate(self):
+		if self.floorPlan.ahu is None:
+			raise ValueError("AHU must be set in floor plan before routing ducts")
+
+		self.pathfinder = Pathfinder(self.floorPlan)
+		furthest_room = self.pathfinder.find_furthest_room(self.floorPlan.ahu)
+		self.goal = furthest_room.center
+
+		print(f"AHU position: {self.floorPlan.ahu.position}")
+		print(f"Furthest room center: {furthest_room.center}")
+		# Create route to the furthest room using optimized A* pathfinding
+		self.nodes = self.pathfinder.a_star(self.floorPlan.ahu.position, furthest_room.center, ax=self.axes)
 	
-	print(f"AHU position: {floor_plan.ahu.position}")
-	print(f"Furthest room center: {furthest_room.center}")
-	
-	fig, ax = plt.subplots(figsize=(12, 8))
-	visualize_layout(floor_plan, ax)
-	
-	# Create route to the furthest room using optimized A* pathfinding
-	index_route, costs = pathfinder.a_star(floor_plan.ahu.position, furthest_room.center, ax=ax, test_name=test_name)
-	visualize_routing([(index_route, costs)], ax, test_name=test_name)
-	
-	plt.draw()
-	plt.show(block=True)  # This will keep the window open even if the test fails
-	return [(index_route, costs)], fig, ax
+	def visualize(self, test_name: str=None):
+		self.fig, self.ax = plt.subplots(figsize=(12, 8))
+		visualize_layout(self.floorPlan, self.ax)
+		visualizer = PathfindingVisualizer(self.pathfinder, self.ax)
+		# If open list is empty, algorithm has completed
+		if not visualizer.pathfinder.open_list:
+			if visualizer.open_list.queue and test_name:
+				visualizer.save_figure(test_name)(self, self.ax)
+		plt.draw()
+		plt.show(block=True)  # This will keep the window open even if the test fails
+		return
