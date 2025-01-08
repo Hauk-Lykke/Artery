6fa911904b0b1b54149c640481bff50492453e43
Hauk-Morten <haly@cowi.com>
2025-01-08 09:46:31 +0100

    folder rename

--- src/MEP.py
+++ src/MEP.py
@@ -7,22 +7,22 @@ class AirHandlingUnit:
 		self.position = position
 	
 
-class MEPRoute:
+class Path:
 	'''Class for storing Mechanical, Electrical and Plumbing routes. Each bend/fitting is a node.'''
 	def __init__(self, startNode: Node):
 		self.startNode = startNode
 		self.endNode = None
 		self.nodes = [startNode]
 		self.length = 0
+		self.cost = self.nodes[-1].g
 
 	def add_node(self, node: Node):
 		self.nodes.append(node)
 		self.startNode = node
 
-class MEPBranch(MEPRoute): # Mechanical, Electrical, Plumbing branch
+class Branch(Path): # Mechanical, Electrical, Plumbing branch
 	def __init__(self, startNode: Node, isIndexRoute: bool = False):
 		super().__init__(startNode)
 		self.sub_branches = []
 		self.isIndexRoute = isIndexRoute
 
-	
