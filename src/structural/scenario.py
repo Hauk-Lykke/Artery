67fe5aea68ca5d55b0a8841921fe3fe81339c825
Hauk-Morten <haly@cowi.com>
2025-02-19 09:48:34 +0100

    New Scenario and Scenario2D class with unit tests.

---
+++ src/structural/scenario.py
@@ -0,0 +1,59 @@
+from core import Node
+from routing import Network2D
+from structural.floor_plan import FloorPlan
+import random
+
+'''These classes facilitate optimization based on the different load scenarios.'''
+
+class Scenario:
+	def __init__(self,floorPlan: FloorPlan):
+		if isinstance(floorPlan, FloorPlan):
+			self.floorPlan = floorPlan
+		else:
+			raise ValueError("floorPlan must be of type FloorPlan.")
+
+
+class Scenario2D(Scenario):
+	def __init__(self,floorPlan: FloorPlan=None, network: Network2D=None, generateScenario: bool=False):
+		if floorPlan:
+			super().__init__(floorPlan)
+		elif generateScenario:
+			floorPlan = FloorPlan()
+			floorPlan.generate()
+			self.floorPlan = floorPlan
+		if network:
+			self.network = network
+		elif generateScenario:
+			startPoint = random.choice([room.center for room in floorPlan.rooms])
+			self.network = Network2D(self.floorPlan, startPoint)
+
+	def evaluate(self):
+		self.network.generate()
+		self.mepCost = max([node.g_cost for node in self.network.nodes])
+		# self.structuralCost = self.wallCost + self.columnCost
+
+class ScenarioOptimization:
+	def __init__(self, scenarios: list[Scenario2D]=None):
+		if scenarios is not None:
+			self.scenarios = scenarios
+		else:
+			self.scenarios = []
+
+	def optimize(self) -> Scenario2D:
+		[scenario.evaluate() for scenario in self.scenarios]
+		costs = [scenario.mepCost for scenario in self.scenarios]
+		index = costs.index(max(costs))
+		return self.scenarios[index]
+	
+	def randomize(self, numberOfCases: int=0):
+		if numberOfCases:
+			self.numberOfCases = numberOfCases
+		if len(self.scenarios):
+			for scenario in self.scenarios:
+				scenario.floorPlan.generate()
+		else:
+			for _ in range(numberOfCases):
+				self.scenarios.append(Scenario2D(None, None, generateScenario=True))
+
+
+
---
+++ tests/test_scenario.py
@@ -0,0 +1,30 @@
+import pytest
+from routing import Network2D
+from structural.floor_plan import FloorPlan
+from structural.scenario import Scenario2D, ScenarioOptimization
+
+@pytest.mark.usefixtures("simple_floor_plan_fixture")
+def test_scenario_create(simple_floor_plan_fixture):
+	network = Network2D(simple_floor_plan_fixture,simple_floor_plan_fixture.rooms[0].center)
+	scenario = Scenario2D(simple_floor_plan_fixture, network)
+	assert(isinstance(scenario, Scenario2D))
+	assert(isinstance(scenario.network, Network2D))
+
+@pytest.mark.usefixtures("simple_floor_plan_fixture")
+def test_scenarioOptimization_optimize(simple_floor_plan_fixture):
+	network = Network2D(simple_floor_plan_fixture,simple_floor_plan_fixture.rooms[0].center)
+	network.generate()
+	scenario = Scenario2D(simple_floor_plan_fixture, network)
+	scenario.evaluate()
+	scenarioOptimization = ScenarioOptimization([scenario])
+	result = scenarioOptimization.optimize()
+	assert(isinstance(result, Scenario2D))
+
+# @pytest.mark.usefixtures("complex_floor_plan_fixture")
+# def test_scenarioOptimization_randomize(complex_floor_plan_fixture):
+
+def test_scenarioOptimization_randomize():
+	scenarioOptimization = ScenarioOptimization()
+	scenarioOptimization.randomize(2)
+	optimalScenario = scenarioOptimization.optimize()
+	assert(isinstance(optimalScenario,Scenario2D))
